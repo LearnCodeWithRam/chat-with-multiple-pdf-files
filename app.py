f0178d9f740c3099c286fe8d386da0b2430eb2a9
Ramanuj <87684078+LearnCodeWithRam@users.noreply.github.com>
2023-10-29 13:07:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+from PIL import Image
 from pyngrok import ngrok
 import os
 #key = os.environ.get('API_Key')
